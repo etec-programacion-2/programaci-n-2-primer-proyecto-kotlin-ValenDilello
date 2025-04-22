300f13552cc7a9d4bc00a83c5952dbf29271d231
ValenDilello <v.dilello@alumno.etec.um.edu.ar>
2025-04-22 12:34:32 -0300

    Configuración inicial

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -4,7 +4,7 @@ import java.time.LocalDate
 fun main() {
     println("¡Hola, Kotlin!")
     primerafuncion()
-    CalcRectagleArea()
+
 }
 
 fun primerafuncion () {
@@ -15,20 +15,4 @@ fun primerafuncion () {
     println ("Fecha: $fechaActual")
 }
 
-fun mostrarTiposDeDatos() {
-    val nombre: String = "Valentino"
-    val edad: Int = 16
-    val altura: Double = 1.71
-    val esEstudiante: Boolean = true
-    
-    println("Nombre: $nombre")
-    println("Edad: $edad")
-    println("Altura: $altura")
-    println("Es estudiante: $esEstudiante")
-}
-fun CalcRectagleArea () {
-    val Base: Double = 5.5
-    val Heigh: Double = 2.2
-    val Area: Double = Base*Heigh
-    println ("El area del rectangulo es: $Area")
-}
+
